0c25f86443e9b1946e8774094cb76b71e85edadb
InevitableDays <umitozkul2015@gmail.com>
2026-02-10 16:01:38 +0300

    Create lang.js

---
+++ js/lang.js
@@ -0,0 +1,19 @@
+export async function loadLanguage(lang) {
+  try {
+    const res = await fetch(`${lang}.json`);
+    const data = await res.json();
+    for (const key in data) {
+      const el = document.getElementById(key);
+      if (el) {
+        if (key === 'workethictext') {
+          el.innerHTML = `<strong>${data[key]}</strong>`;
+        } else {
+          el.textContent = data[key];
+        }
+      }
+    }
+    document.documentElement.lang = lang;
+  } catch (err) {
+    console.error("Error loading language:", err);
+  }
+}
