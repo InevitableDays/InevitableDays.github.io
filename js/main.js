bd640f1fbfe4baedc8fad92032b5aa007fd64e74
InevitableDays <umitozkul2015@gmail.com>
2026-02-11 13:21:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,31 @@ initMinesweeper();
 //player
   const Btn = document.getElementById("Btn");  
   const Music = document.getElementById("Music");
+  const frameElement = document.getElementById("neonFrame"); //Nframe Start
+
+let running = false;
+
+function startClubLights() {
+  frameElement.classList.add("active");
+}
+
+function stopClubLights() {
+  frameElement.classList.remove("active");
+}
+
   Btn.addEventListener("click", () => {
     if (Music.paused) {
       Music.play();
       Btn.style.opacity = "1";
+      if (!running) {       //Nframe btn js 
+      startClubLights();
+      running = true;
+    }                       //Nframe btn js 
     } else {
       Music.pause();
       Btn.style.opacity = "0.65";
+      stopClubLights();      //Nframe btn js 
+    running = false;          //Nframe btn js 
     }
 });
 
